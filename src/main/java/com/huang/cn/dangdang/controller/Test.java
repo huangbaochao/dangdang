5dc00a09574351a9b16dd4e058b6f56b13082e06
huangbc <huangbc_6603@163.com>
2023-05-28 20:19:30 +0800

    mock test
    test assert
    
    test

---
+++ src/main/java/com/huang/cn/dangdang/controller/Test.java
@@ -0,0 +1,7 @@
+package com.huang.cn.dangdang.controller;
+
+/**
+ * @Author: Derek.huang on 2023/5/29 12:47.
+ */
+public class Test {
+}
---
+++ src/test/java/com/huang/cn/dangdang/service/impl/SysUserServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.huang.cn.dangdang.service.impl;
+
+import com.huang.cn.dangdang.entity.SysUser;
+import com.huang.cn.dangdang.service.SysUserService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Derek.huang on 2023/5/28 19:36.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SysUserServiceImplTest {
+
+    @Resource
+    private SysUserService sysUserService;
+
+    @Test
+    public void should_result_success_when_selectUser(){
+
+        SysUser sysUser = sysUserService.queryById("888");
+        Assert.assertEquals("derek.huang",sysUser.getLoginName());
+    }
+
+
+}
